e3b749eff13b764f4088072b5632360a4e85da4d
Spinelli666 <bernardospinelli@hotmail.com.br>
2025-01-13 19:55:20 -0300

    (wip): Resgatando todos os serviços

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -19,7 +19,17 @@ const serviceController = {
         } catch (error) {
             console.log(error)
         }
-    }
+    },
+    getAll: async(req, res) => {
+        try {
+            const services = await ServiceModel.find()
+
+            res.json(services)
+
+        } catch (error) {
+            console.log(error)
+        }
+    },
 }
 
 module.exports = serviceController;
