c0fdf94ca8ed9d6fcdd04907c9f1120951dbf6ad
Spinelli666 <bernardospinelli@hotmail.com.br>
2025-01-13 17:34:39 -0300

    (wip): Inserindo dados no banco

--- backend/routes/services.js
+++ backend/routes/services.js
@@ -4,4 +4,6 @@ const serviceController = require('../controllers/serviceController')
 
 // Function
 
+router.route('/services').post((req, res) => serviceController.create(req, res))
+
 module.exports = router
