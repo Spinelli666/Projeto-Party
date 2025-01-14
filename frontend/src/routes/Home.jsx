42fa5d1c5236f1216ae3bea51254e0f9382c305e
Spinelli666 <bernardospinelli@hotmail.com.br>
2025-01-14 16:36:09 -0300

    (wip): Resgatando dados da festa na home

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import partyFetch from "../axios/config"
+
+import { useState, useEffect } from "react"
+
+import { Link } from "react-router-dom"
 
 const Home = () => {
+
+    const [parties, setParties] = useState(null)
+
+    // Load parties
+
+    useEffect(() => {
+        const loadParties = async () => {
+            const res = await partyFetch.get('/parties')
+
+            console.log(res)
+    
+            setParties(res.data)
+        }
+
+        loadParties()
+    }, [])
+
+    if(!parties) return <p>Carregando...</p>
+
   return (
-    <div>
-      Home
+    <div className="Home">
+      <h1>Suas Festas</h1>
+      <div className="parties-container">
+        {parties.lenght === 0 && <p>Você ainda não tem festas</p>}
+        {parties.map(party => (
+          <div className="party" key={party._id}>
+            <img src={party.image} alt={party.title} />
+            <h3>{party.title}</h3>
+            <Link to={`/party/${party._id}`} className="btn-secondary">Detalhes</Link>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
