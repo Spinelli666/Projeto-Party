aa5010744529366ced17c09a101865c9f145bfaf
Spinelli666 <bernardospinelli@hotmail.com.br>
2025-01-14 19:12:53 -0300

    (wip): DELETE PARTY

--- frontend/src/routes/Party.jsx
+++ frontend/src/routes/Party.jsx
@@ -1,50 +1,69 @@
-import partyFetch from '../axios/config'
+import partyFetch from "../axios/config";
 
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-import { useParams, Link, useNavigate } from 'react-router-dom'
+import { useParams, Link, useNavigate } from "react-router-dom";
+import useToast from "../hooks/useToast";
+
+import "./Party.css";
 
 const Party = () => {
+  const { id } = useParams();
 
-  const {id} = useParams()
+  const [party, setParty] = useState(null);
 
-  const [party, setParty] = useState(null)
+  const navigate = useNavigate();
 
   // Load party
-
   useEffect(() => {
     const loadParty = async () => {
-      const res = await partyFetch.get(`/parties/${id}`)
+      const res = await partyFetch.get(`/parties/${id}`);
 
-      console.log(res.data)
+      setParty(res.data);
+    };
 
-      setParty(res.data)
+    loadParty();
+  }, []);
+
+  //  Delete this party
+  const handleDelete = async () => {
+    const res = await partyFetch.delete(`/parties/${id}`);
+
+    if (res.status === 200) {
+      navigate("/");
+
+      useToast(res.data.msg);
     }
-
-    loadParty()
-  }, [])
-
-  if(!party) return <div>Carregando...</div>
+  };
 
   return (
-    <div className='party'>
-      <h1>{party.title}</h1>
-      <div className="actions-container">
-        <Link className='btn'>Editar</Link>
-        <button className='btn-secondary'>Excluir</button>
-        <p>Orçamento: R${party.budget}</p>
-        <h3>Serviços contratados:</h3>
-        <div className="services-container">
-          {party.services.map((service) => (
-            <div className='service' key={service._id}>
-              <img src={service.img} alt={service.name} />
-              <p>{service.name}</p>
-            </div>
-          ))}
+    <div>
+      {!party && <p>Carregando...</p>}
+      {party && (
+        <div className="party">
+          <h1>{party.title}</h1>
+          <div className="actions-container">
+            <Link to={`/party/edit/${party._id}`} className="btn">
+              Editar
+            </Link>
+            <button onClick={handleDelete} className="btn-secondary">
+              Excluir
+            </button>
+          </div>
+          <p>Orçamento: R${party.budget}</p>
+          <h3>Serviços contratados:</h3>
+          <div className="services-container">
+            {party.services.map((service) => (
+              <div className="service" key={service._id}>
+                <img src={service.image} alt={service.name} />
+                <p>{service.name}</p>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Party
+export default Party;
